de8eec0203333177b58d4ba7abde5be132e32de3
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-04 01:03:16 +0530

    practice

--- JspidersJAVANEW/src/box8OOPS/Hackthon.java
+++ JspidersJAVANEW/src/box8OOPS/Hackthon.java
@@ -1,5 +1,14 @@
 package box8OOPS;
 
+class p1 {
+	int a = 3888;// non static global variable
+	static String b = "pranav"; // Static global variable
+
+	static void person() { // non static method
+		System.out.println("Hello from pranav");
+	}
+}
+
 public class Hackthon {
 	int num; // Global variable have default value concept
 	String s;
@@ -7,11 +16,14 @@ public class Hackthon {
 
 	public static void main(String[] args) {
 		System.out.println("Main Start");
-		int a;// local variable do not have default value concept
+		// int a;// local variable do not have default value concept
 //	System.out.println(a); //CTE
 		System.out.println(new Hackthon().num);// 0 Default value
 		System.out.println(new Hackthon().s); // null Default value
 		System.out.println(Hackthon.d);// 0.0 Default value
+		System.out.println(new p1().a);
+		System.out.println(p1.b);
+		p1.person();
 		System.out.println("Main End");
 	}
 }
