ac4760541fc74cd0a2fd9b74210b00fd721d2e97
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-14 00:02:47 +0530

    practice

---
+++ JspidersJAVANEW/src/box14Encapuslation/PapaMeriJaan.java
@@ -0,0 +1,22 @@
+package box14Encapuslation;
+
+class father {
+	void gold() {
+		System.out.println("10kg Gold");
+	}
+}
+
+class Abhijit extends father {
+	void ganja() {
+		System.out.println("Ganja Fukna");
+	}
+}
+
+public class PapaMeriJaan {
+	public static void main(String[] args) {
+
+		Abhijit a1 = new Abhijit();
+		a1.gold();
+		a1.ganja();
+	}
+}
