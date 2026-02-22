4eae11f194354623c37bd33cb871842e34dbe07c
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-22 19:40:16 +0530

    method overloading in same class - 2

--- JspidersJAVANEW/src/box15Inheritance/Practice.java
+++ JspidersJAVANEW/src/box15Inheritance/Practice.java
@@ -1,12 +1,17 @@
 package box15Inheritance;
 
+//method overloading code
 class hw {
 	void info(double b, char ch) {
-		System.out.println(" Marks is " + b + " Gen is " + ch);
+		System.out.println("Marks is " + b + " Gen is " + ch);
 	}
 
 	void info(String str, int no) {
 		System.out.println("Student name is " + str + " Roll no is " + no);
+	}
+
+	void info(int a, double d, String str, char ch) {
+		System.out.println(+a + d + str + ch);
 	}
 }
 
@@ -16,5 +21,7 @@ public class Practice {
 		h1.info(100.0, 'M');
 		System.out.println("---------------------------");
 		h1.info("Don", 100);
+		System.out.println("---------------------------");
+		h1.info(10, 500.0, " pranav ", 'S');
 	}
 }
