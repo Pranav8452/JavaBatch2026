72b19556027cc0e2347118ef117e7a1ae60b0d08
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-24 00:08:09 +0530

    UPcasting

--- JspidersJAVANEW/src/box15Inheritance/Ranibagh.java
+++ JspidersJAVANEW/src/box15Inheritance/Ranibagh.java
@@ -2,7 +2,7 @@ package box15Inheritance;
 
 //Method OverRiding and with Hierarchical Example
 
-class Animal {
+class Animal { // Method Overriding
 	void eat() {
 		System.out.println("Animal eat food");
 	}
@@ -21,13 +21,46 @@ class Animal {
 }
 
 class dog extends Animal {
+	@Override
 	void sound() {
 		System.out.println("bhaw...bhaw...");
 	}
 }
 
+class cat extends Animal {
+	@Override
+	void sound() {
+		System.out.println("Meow...Meow...");
+	}
+}
+
+class snake extends Animal {
+	@Override
+	void sound() {
+		System.out.println("sss...sss...");
+	}
+}
+
 public class Ranibagh {
 	public static void main(String[] args) {
+		Animal a1 = new dog();// UPcasting
+		a1.sound();
+		a1.eat();
+		a1.sleep();
+		a1.drink();
+
+		System.out.println("-------------");
+		Animal a2 = new cat();// UPcasting
+		a2.sound();
+		a2.eat();
+		a2.sleep();
+		a2.drink();
+		System.out.println("-------------");
+		Animal a3 = new snake();// UPcasting
+		a3.sound();
+		a3.eat();
+		a3.sleep();
+		a3.drink();
 
 	}
 }
