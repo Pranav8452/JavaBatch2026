ec0eb3345b79e12af531e0846825f845af746345
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-25 20:35:47 +0530

    UPcasting,Downcasting

---
+++ JspidersJAVANEW/src/box16TypeCasting/FamilyGuy.java
@@ -0,0 +1,39 @@
+package box16TypeCasting;
+
+//Type Casting Example 
+class father {
+	int f = 5000;
+
+	void work() {
+		System.out.println("Hard Working......");
+	}
+}
+
+class bhushan extends father { // subclass
+	int b = 700;
+
+	void study() {
+		System.out.println("Studying.....");
+	}
+}
+
+public class FamilyGuy {
+	public static void main(String[] args) {
+
+		father f1 = new bhushan(); // Upcasting
+		System.out.println(f1.f);
+		f1.work();
+		// System.out.println(f1.b);// CTE
+		// f1.study();// CTE
+		// sub class properties are hidden
+
+		System.out.println("_________________________");
+
+		bhushan b1 = (bhushan) f1;
+		System.out.println(b1.f);// Downcasting
+		b1.work();
+		System.out.println(b1.b);
+		b1.study();
+
+	}
+}
