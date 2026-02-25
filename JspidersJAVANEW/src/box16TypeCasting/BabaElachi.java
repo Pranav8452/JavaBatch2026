234fa1a1919bffa42ef16156ee217c472e2b3622
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-25 23:50:42 +0530

    upcssting  2, downcasting 2

--- JspidersJAVANEW/src/box16TypeCasting/BabaElachi.java
+++ JspidersJAVANEW/src/box16TypeCasting/BabaElachi.java
@@ -24,17 +24,24 @@ public class BabaElachi {
 	public static void main(String[] args) {
 		System.out.println("Main Start");
 
-		baba b1 = new porgi();// Upcasting
+		baba b1 = new porgi();// Upcasting-1
 		b1.property();
 
 		System.out.println("--------------");
 
-		porgi p1 = (porgi) b1;// Downcasting
+		porgi p1 = (porgi) b1;// Downcasting-1
 		p1.makeup();
 		b1.property();
 		System.out.println("---------------");
 		// ClassCastException
 
+		baba b2 = new mulga(); // Upcasting-2
+		System.out.println("****************");
+		b2.property();
+
+		mulga m1 = (mulga) b2;// Downcasting-2
+		m1.property();
+		m1.bike();
 		System.out.println("Main End");
 	}
 }
