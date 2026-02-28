f1d26148c3969b0336f22b08dffee85f76db3166
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-28 21:13:14 +0530

    Practice qn

--- JspidersJAVANEW/src/box17genANDspe/Election.java
+++ JspidersJAVANEW/src/box17genANDspe/Election.java
@@ -3,12 +3,23 @@ package box17genANDspe;
 public class Election {
 	public static void main(String[] args) {
 		Person p1 = new Person("Pranav", 22, 'm');
-		Student s1 = new Student("bushan", 20, 'M', 43, 73.75, "BRHCE");
-		Employee e1 = new Employee("Abhijit", 25, 'M', 800, 1200000, "Developer");
+		Person p2 = new Person("Saras", 22, 'm');
+		Student s1 = new Student("sahil", 22, 'M', 43, 59.75, "BRHCE");
+		Student s2 = new Student("Siddhant", 22, 'M', 43, 73.75, "BRHCE");
+		Employee e1 = new Employee("Abhijit", 25, 'M', 800, 50000, "Developer");
+		Employee e2 = new Employee("Saish", 25, 'M', 800, 1200000, "Developer");
 
 		Goverment g1 = new Goverment();
 		g1.Aadharcard(p1);
+		System.out.println("--------------------");
 		g1.Scholrship(s1);
+		System.out.println("--------------------");
 		g1.TaxPayer(e1);
+		System.out.println("----------------------");
+		g1.Aadharcard(p2);
+		System.out.println("----------------------");
+		g1.Scholrship(s2);
+		System.out.println("----------------------");
+		g1.TaxPayer(e2);
 	}
 }
