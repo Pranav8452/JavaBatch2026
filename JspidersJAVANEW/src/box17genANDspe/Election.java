2415cd5add57b0609d87c7c8a8d2656700ac6b89
PranavSurve-dev <pranavsurve444@gmail.com>
2026-02-28 21:08:08 +0530

    important Qn

--- JspidersJAVANEW/src/box17genANDspe/Election.java
+++ JspidersJAVANEW/src/box17genANDspe/Election.java
@@ -3,12 +3,12 @@ package box17genANDspe;
 public class Election {
 	public static void main(String[] args) {
 		Person p1 = new Person("Pranav", 22, 'm');
-//		Student s1 = new Student();
-//		Employee e1 = new Employee();
+		Student s1 = new Student("bushan", 20, 'M', 43, 73.75, "BRHCE");
+		Employee e1 = new Employee("Abhijit", 25, 'M', 800, 1200000, "Developer");
 
 		Goverment g1 = new Goverment();
-		g1.AadharCard(p1);
-//		g1.Scholrship(p1);
-//		g1.TaxPayer(p1);
+		g1.Aadharcard(p1);
+		g1.Scholrship(s1);
+		g1.TaxPayer(e1);
 	}
 }
--- JspidersJAVANEW/src/box17genANDspe/Goverment.java
+++ JspidersJAVANEW/src/box17genANDspe/Goverment.java
@@ -3,17 +3,26 @@ package box17genANDspe;
 public class Goverment {
 
 //Generalization Method
-	void AadharCard(Person p) {
+	void Aadharcard(Person p) {
 
-		System.out.println("Everyone want Aadhar card");
+		System.out.println(p.name + " Will get Aadharcard");
 	}
 
 //Specialization Method
 	void Scholrship(Student s) {
-		System.out.println("Student got Scholrship");
+		if (s.per >= 60) {
+			System.out.println(s.name + " got Scholrship");
+		} else {
+			System.out.println(s.name + " better luck next time(u want above 60)");
+		}
 	}
 
 	void TaxPayer(Employee e) {
-		System.out.println("Employee Should pay Income tax");
+		if (e.sal * 12 > 1200000) {
+			System.out.println(e.name + " you should pay tax");
+			System.out.println("Tax payable is " + e.sal * 12 * 0.18 + " rs/- only");
+		} else {
+			System.out.println(e.name + " tax dene ki zarurat nahi hya");
+		}
 	}
 }
