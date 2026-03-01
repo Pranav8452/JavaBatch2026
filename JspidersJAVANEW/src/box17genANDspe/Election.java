3b640d92e792fbdab921f29dc4ce6cbc725581ab
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-01 21:54:06 +0530

    Daily Code

--- JspidersJAVANEW/src/box17genANDspe/Election.java
+++ JspidersJAVANEW/src/box17genANDspe/Election.java
@@ -4,6 +4,7 @@ public class Election {
 	public static void main(String[] args) {
 		Person p1 = new Person("Pranav", 22, 'm');
 		Person p2 = new Person("Saras", 22, 'm');
+		Person p3 = new Person("Surve", 21, 'M');
 		Student s1 = new Student("sahil", 22, 'M', 43, 59.75, "BRHCE");
 		Student s2 = new Student("Siddhant", 22, 'M', 43, 73.75, "BRHCE");
 		Employee e1 = new Employee("Abhijit", 25, 'M', 800, 50000, "Developer");
@@ -21,5 +22,7 @@ public class Election {
 		g1.Scholrship(s2);
 		System.out.println("----------------------");
 		g1.TaxPayer(e2);
+		System.out.println("***********");
+		g1.Aadharcard(p3);
 	}
 }
