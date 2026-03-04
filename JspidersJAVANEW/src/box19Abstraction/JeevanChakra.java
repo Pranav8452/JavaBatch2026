6124364ad8b145c64cd1699266c396251eba2689
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-04 21:16:00 +0530

    practice makes man perfect

---
+++ JspidersJAVANEW/src/box19Abstraction/JeevanChakra.java
@@ -0,0 +1,25 @@
+package box19Abstraction;
+
+abstract class swarg {// abstract class
+
+	abstract void rambha();// abstract method
+
+	abstract void urvashi();
+
+	abstract void menka();
+
+	void narad() { // concrete method
+		System.out.println("naryanaaaaaa....... naryanaa.....");
+	}
+}
+
+abstract class indra extends swarg { // concrete class
+
+}
+
+public class JeevanChakra {
+	public static void main(String[] args) {
+
+	}
+
+}
