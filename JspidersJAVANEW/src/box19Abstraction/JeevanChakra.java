69d9234da1b0da888eec4833780afa792dfab4c7
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-04 21:20:36 +0530

    Abstraction example

--- JspidersJAVANEW/src/box19Abstraction/JeevanChakra.java
+++ JspidersJAVANEW/src/box19Abstraction/JeevanChakra.java
@@ -14,12 +14,30 @@ abstract class swarg {// abstract class
 }
 
 abstract class indra extends swarg { // concrete class
+	@Override
+	void rambha() {
+		System.out.println("Rambha ka Dance...........");
+	}
 
+	@Override
+	void urvashi() {
+		System.out.println("urvashi ka Dance...........");
+	}
+
+}
+
+class sunil extends indra {
+	void menka() {
+		System.out.println("menka ka Dance...........");
+	}
 }
 
 public class JeevanChakra {
 	public static void main(String[] args) {
-
+		swarg s1 = new sunil();
+		s1.rambha();
+//		s1.rambha();
+//		s1.urvashi();
 	}
 
 }
