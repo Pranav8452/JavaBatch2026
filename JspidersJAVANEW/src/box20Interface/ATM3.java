84be52ac6e258a8c458a00aeab7740090fbbf4bf
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-06 22:01:31 +0530

    practice-1

--- JspidersJAVANEW/src/box20Interface/ATM3.java
+++ JspidersJAVANEW/src/box20Interface/ATM3.java
@@ -1,5 +1,9 @@
 package box20Interface;
 
 public interface ATM3 {
+	public abstract void deposit(int amt1);
 
+	public abstract void withdraw(int amt2);
+
+	public abstract void checkBal();
 }
--- JspidersJAVANEW/src/box20Interface/SBIBank.java
+++ JspidersJAVANEW/src/box20Interface/SBIBank.java
@@ -1,5 +1,32 @@
 package box20Interface;
 
-public class SBIBank {
+public class SBIBank implements ATM1, ATM2, ATM3 {
+
+	int totalBal;// Non-static global variable
+
+	@Override
+	public void deposit(int amt1) {
+		if (amt1 > 0) {
+			totalBal += amt1;
+			System.out.println(amt1 + "Deposited Successful");
+		} else {
+			System.out.println("Enter Valid Deposit Amount");
+		}
+	}
+
+	@Override
+	public void withdraw(int amt2) {
+		if (amt2 <= totalBal) {
+			totalBal -= amt2;
+			System.out.println(amt2 + "Withdraw Successful");
+		} else {
+			System.out.println("Tera bap chod ke gya tha ya teri maa");
+		}
+	}
+
+	@Override
+	public void checkBal() {
+		System.out.println("Total Avaliable bal is " + totalBal);
+	}
 
 }
