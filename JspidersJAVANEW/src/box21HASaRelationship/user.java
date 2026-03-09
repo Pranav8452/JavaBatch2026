73225e01e6bb29e78a144bf6ebf8d1b001770784
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-09 20:26:11 +0530

    HAS-A Relationship

---
+++ JspidersJAVANEW/src/box21HASaRelationship/user.java
@@ -0,0 +1,31 @@
+package box21HASaRelationship;
+
+class battery {
+	int x = 255;
+
+//Overloaded
+	void capacity() {
+		System.out.println("7000mah battery");
+	}
+
+	void capacity(int a) {
+		System.out.println("Colling System...");
+	}
+}
+
+class mobile {
+	static battery b1 = new battery();
+	int a = 96;
+
+	void games() {
+		System.out.println("Shadow Fight,Mini Millatia");
+	}
+}
+
+public class user {
+	public static void main(String[] args) {
+		mobile m1 = new mobile();
+		System.out.println(m1.b1.x);// call variable
+		m1.b1.capacity();
+	}
+}
