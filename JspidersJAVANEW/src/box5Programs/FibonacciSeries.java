1590e056820838f2815fcea859a01303f45600d7
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-15 15:02:36 +0530

    some changes

--- JspidersJAVANEW/src/box5Programs/FibonacciSeries.java
+++ JspidersJAVANEW/src/box5Programs/FibonacciSeries.java
@@ -4,7 +4,7 @@ public class FibonacciSeries {
 	public static void main(String[] args) {
 		int n1 = 0;
 		int n2 = 1;
-		int limit = 10;
+		int limit = 12;
 
 		System.out.print(n1 + " " + n2 + " ");
 		for (int i = 3; i <= limit; i++) {
