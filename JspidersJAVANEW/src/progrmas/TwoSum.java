e8d06c232d992d76e03d4de91c7b0066ac9e4ce9
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-15 15:01:40 +0530

    changes

--- JspidersJAVANEW/src/progrmas/TwoSum.java
+++ JspidersJAVANEW/src/progrmas/TwoSum.java
@@ -5,11 +5,11 @@ import java.util.HashMap;
 public class TwoSum {
 	public static void main(String[] args) {
 		int[] nums = { 2, 7, 11, 15 };
-		int target = 9;
+		int n1 = 9;
 		HashMap<Integer, Integer> map = new HashMap<>();
 
 		for (int i = 0; i < nums.length; i++) {
-			int complement = target - nums[i];
+			int complement = n1 - nums[i];
 			if (map.containsKey(complement)) {
 				System.out.println("Indices: " + map.get(complement) + " , " + i);
 			}
